510f671052cfcfdc5227ce0fa7267068b88c753c
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2023-11-11 18:48:01 +0100

    make fancy title

--- Scenes/Ui/TitleScreen.gd
+++ Scenes/Ui/TitleScreen.gd
@@ -1,16 +1,14 @@
 extends Control
 
+@onready var level = preload(Autoload.level)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func _on_about_button_pressed():
+	OS.shell_open("https://github.com/emmdie/3m5JamGame2023")
+	
+func _on_quit_button_pressed():
+	get_tree().quit()
 
 func _on_play_button_pressed():
-	
-	pass # Replace with function body.
+	level = level.instantiate()
+	get_tree().root.add_child(level)
+	queue_free()
--- Scripts/Autoload.gd
+++ Scripts/Autoload.gd
@@ -1,5 +1,5 @@
 extends Node
-
+const level = "res://Scenes/Dungeon/DungeonRoom_New.tscn"
 const CELLSIZE := 64
 
 var filled_cells := {Vector2.ZERO:0}
