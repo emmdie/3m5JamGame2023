cdee770cabc457682f891d3fc7dd79ab8a486538
vf26xawe <vf26xawe@studserv.uni-leipzig.de>
2023-11-11 17:00:35 +0100

    Tilemap Colission gefixt

--- Scripts/Character/PlayerHealth.gd
+++ Scripts/Character/PlayerHealth.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready
-var max_health = PlayerStats.MAXHEALTH
+var max_health = PlayerStats.max_health
 var current_health
 
 # Called when the node enters the scene tree for the first time.
