999f9cc24c4e16ab2d8f930126179e9a3ef56db1
Emmanuel Diehl <emmanuel.diehl@ifsr.de>
2023-11-12 12:14:55 +0100

    implement pickups

--- Scripts/Items/hp_pickup.gd
+++ Scripts/Items/hp_pickup.gd
@@ -1,10 +1,17 @@
 extends Control
 
-const hp = 3
+var hp = 3
 
 func pickup():
 	PlayerStats.current_health += hp
 	$PickupSound.play()
+	hp = 0
+	visible = false
 	
 func _on_pickup_sound_finished():
 	queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	if area.has_method("stretch_tongue"):
+		pickup()
